78db7c212b7fa78a044887608e920a59f1a89153
nikam <nika.macharadze@gmail.com>
2021-04-22 22:06:51 +0400

    test1

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get("/name/{my_name}")
+def read_item(my_name: str):
+    return {"name": my_name}
+
+
 @app.get("/")
 async def root():
     return {"message": "zd kowik"}
