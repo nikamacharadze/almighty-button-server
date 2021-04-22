9754818a818890330fddd0d8e957b9f783e7e5f7
nikam <nika.macharadze@gmail.com>
2021-04-22 22:33:43 +0400

    Enable CORS

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/name/{my_name}")
-def read_item(my_name: str):
+def read_item(my_name: str, my_surname: str):
     return {"name": my_name}
 
 
