1c222b114405740e20999201baec931fd9c5c301
nikam <nika.macharadze@gmail.com>
2021-04-23 16:28:37 +0400

    travis change2

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+# from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 
-origins = ["*"]
+# origins = ["*"]
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-
-@app.get("/chat/{my_name}/{text}")
-a = {}
-def read_item(my_name: str, text: str):
-    a[my_name] = text
-    return a
-
-
-@app.get("/name/{my_name}")
-def read_item(my_name: str):
-    return {"name": my_name}
+# a = {}
+#
+# @app.get("/chat/{my_name}/{text}")
+# def read_item(my_name: str, text: str):
+#     a[my_name] = text
+#     return a
+#
+#
+# @app.get("/name/{my_name}")
+# def read_item(my_name: str):
+#     return {"name": my_name}
 
 
 @app.get("/")
